afc1fb3aa3ebb5ac60715528432d29d0006f9c66
swom <swom@bemily.com>
2025-07-01 13:13:53 +0900

    Revert "feat : flutter sdk update error fix"
    This reverts commit c6add8750a9a213c266115b99a930d08912f3bc4.

--- lib/src/image_crop.dart
+++ lib/src/image_crop.dart
@@ -10,7 +10,7 @@ class ImageOptions {
   });
 
   @override
-  int get hashCode => Object.hash(width, height);
+  int get hashCode => hashValues(width, height);
 
   @override
   bool operator ==(other) =>
